4b4459f7a8dfdd39028627fadc688abe0c3bad60
natcafd <65788496+natcafd@users.noreply.github.com>
2024-08-28 19:18:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const texts = [
+    "sayangg acaa minta maaf",
+    "acaa janjii enggak gitu lagi ke ayang",
+    "acaa nakal udah ngomong gitu ke ayang",
+    "gakk mauu lagi bikin ndaa nangiss",
+    "ndaaaa mauu kann maaafin acaa",
+    "gak mau?",
+    "nangis acaa kalo gak mauu",
+    "heheehe gakk kokkk"
+];
+
+let currentIndex = 0;
+
+const textContainer = document.getElementById('textContainer');
+const textDisplay = document.getElementById('textDisplay');
+
+textContainer.addEventListener('click', () => {
+    textDisplay.classList.remove('fade-in');
+    textDisplay.classList.add('fade-out');
+    
+    setTimeout(() => {
+        if (currentIndex < texts.length) {
+            textDisplay.textContent = texts[currentIndex];
+            currentIndex++;
+        } else {
+            textDisplay.textContent = "ituu ajaa ya sayangg, i love youu";
+            currentIndex = 0; // Optional: Reset the sequence if you want to restart
+        }
+
+        textDisplay.classList.remove('fade-out');
+        textDisplay.classList.add('fade-in');
+    }, 500); // Timing matches the CSS animation duration
+});
